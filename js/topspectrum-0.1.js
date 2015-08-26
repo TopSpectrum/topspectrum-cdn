d65973cfa91aaff0c09fef4e7762ca37b1b9ee85
msmyers <michael@topspectrum.com>
2015-08-26 11:14:28 -0700

    Modernizr (only detecting History support) (js) Ts methods copied over from myaccount to central lib.

--- js/topspectrum-0.1.js
+++ js/topspectrum-0.1.js
@@ -9,7 +9,46 @@
  * @type {{}}
  */
 var Ts = {
-    emptyFn: function() {}
+    ensureFn: function(fn) {
+        if (!fn) {
+            return this.emptyFn;
+        }
+
+        return fn;
+    },
+
+    emptyFn: function() {
+
+    },
+
+    namespace : function() {
+
+        var argsLen = arguments.length;
+
+        for(var i = 0; i < argsLen; i++){
+            var arg = arguments[i];
+            if (!_.isString(arg))
+                continue;
+
+            var parts = arg.split('.');
+
+            if (!parts) {
+                return;
+            }
+
+            var partsLen = parts.length;
+            var obj = window;
+            for(var j = 0; j < partsLen; j++){
+                var thing = obj[parts[j]];
+
+                if (!thing) {
+                    thing = obj[parts[j]] = {};
+                }
+
+                obj = thing;
+            }
+        }
+    }
 };
 
 (function() {
