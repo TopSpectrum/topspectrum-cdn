ccd1997e989d661530656fa60ebb2edd5056e588
msmyers <michael@topspectrum.com>
2015-08-27 19:00:01 -0700

    working on historylet

--- js/topspectrum-0.1.js
+++ js/topspectrum-0.1.js
@@ -128,43 +128,113 @@ Ts.Plugin = Ts.Object.extend({
  */
 Ts.Historylet = (new Ts.Object({
 
+    watches: {},
+
+    current: null,
+
+    history: null,
+
+    onPopState : function() {
+        if (!Modernizr.history || !this.history) {
+            return;
+        }
+
+        var controller;
+
+        if (this.current) {
+            controller = this.popCurrentController();
+
+            if (controller && !controller.cancelled) {
+                controller.trigger('deactivate');
+            }
+        }
+
+        var state = this.history.state;
+
+        if (state) {
+            state = state.number;
+        }
+
+        controller = this.watches[state];
+
+        if (controller && !controller.cancelled) {
+            controller.trigger('activate');
+            this.current = controller.num;
+        }
+    },
+
+    bind: function(history) {
+        this.history = history;
+
+        return this.history;
+    },
+
+    getCurrentController : function() {
+        return this.watches[this.current];
+    },
+
+    popCurrentController: function() {
+        var controller = this.watches[this.current];
+        delete this.watches[this.current];
+        delete this.current;
+        return controller;
+    },
+
     /**
      * Register a fleeting
      *
      * @param callback
      * @return controller object
      */
-    push: function(callback) {
+    push: function() {
 
-        history.pushState({number: Math.random()}, window.title, window.url);
+        var num = Math.random();
+        var that = this;
 
-        return new Ts.Object({
+        var result = new Ts.Object({
 
-            _cancelled: false,
+            cancelled: false,
+
+            num: num,
 
             initialize: function() {
-                this.listenToOnce($(window), 'popstate', $.proxy(this._popstate, this));
-            },
 
-            _popstate: function() {
-                if (this._cancelled) {
-                    return;
-                }
-
-                // TODO: check to see if it's our current state?
-
-                Ts.ensureFn(callback)();
             },
 
             cancel: function() {
-                if (this._cancelled) {
+                if (this.cancelled) {
                     return;
+                } else if (this !== that.getCurrentController()) {
+                    throw 'Only can cancel current one with this implementation';
                 }
 
-                this._cancelled = true;
+                this.cancelled = true;
                 this.trigger('cancelled');
             }
         });
+
+        this.watches[num] = result;
+
+        if (this.history) {
+            var controller = this.getCurrentController();
+
+            if (controller && controller.cancelled) {
+                this.history.replaceState({number: num}, window.title, window.url);
+            } else {
+                this.history.pushState({number: num}, window.title, window.url);
+            }
+
+            result.pushed = true;
+        } else {
+            result.pushed = false;
+        }
+
+        this.current = num;
+
+        return result;
     }
 
 }));
+
+Ts.Historylet.bind(window.history);
+$(window).on('popstate', $.proxy(Ts.Historylet.onPopState, Ts.Historylet));
