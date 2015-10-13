6dc62da692ed9270bb5131ba4c2576e4f761f297
EC2 Default User <ec2-user@ip-172-31-40-131.us-west-2.compute.internal>
2015-10-13 17:32:08 +0000

    Fixes to move to PHP, and abstraction of HEADER and FOOTER sections

---
+++ nic.feedback/faq/index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+
+<html>
+
+<?php include("../shared/header.html"); ?>
+
+<body>
+
+<section class="home-hero">
+    <nav class="navbar" style="position: relative;">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                        data-target="#navbar" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+                <a href="/" class="navbar-brand">.feedback</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="navbar">
+                <ul class="nav navbar-nav navbar-right">
+                    <!--<li class="active"><a href="#">Packages</a></li>-->
+                    <!--<li class="active"><a href="#">Customers</a></li>-->
+                    <!--<li class="active"><a href="#">Pricing</a></li>-->
+                    <!--<li class="active"><a href="#">Log In</a></li>-->
+                    <li class="active"><a href="/registrars">Registrars</a></li>
+                    <li class="active"><a href="/contact">Contact</a></li>
+                    <li class="active"><a href="//www.myservice.feedback/login">Login</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="home-hero-top">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="section-headline" style="margin-bottom: 33px;">
+                        <span class="shadow-text">Frequently Asked Questions</span>
+                    </h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="panel">
+                    <div class="panel-body" style="color: black;">
+                        <section id="module-faq" class="module-contact text-inverse">
+                            <ul>
+                                <li>asdf</li>
+                            </ul>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="home-hero-bottom" style="">
+        <div class="module-customer-logos" style="">
+
+            <?php include("shared/footer.html"); ?>
+
+        </div>
+    </div>
+</section>
+
+<script type="application/javascript" src="//cdn.feedback/js/jquery-1.11.2.js"></script>
+<script type="application/javascript" src="//cdn.feedback/js/bootstrap-3.3.5.js"></script>
+
+</body>
+</html>
--- nic.feedback/registrars/index.php
+++ nic.feedback/registrars/index.php
@@ -1,53 +1,8 @@
 <!DOCTYPE html>
 
 <html>
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
-    <meta charset="utf-8">
-
-    <title>.feedback Registry</title>
-
-    <!-- Behavioral Meta Data -->
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <!-- Core Meta Data -->
-    <meta name="author" content="Top Level Spectrum">
-    <meta name="description"
-          content="Top Level Spectrum">
-    <meta name="keywords" content="gtld,tld,feedback,forum,realty,contact,pid,gtld">
-
-    <!-- Humans -->
-    <link rel="author" href="/humans.txt"/>
-    <link rel="shortcut icon" href="/assets/favicon.ico" type="image/x-icon"/>
-
-    <!-- Open Graph Meta Data -->
-    <meta property="og:description"
-          content=".feedback gTLD Registry">
-    <meta property="og:image" content="">
-    <meta property="og:site_name" content=".feedback Registry">
-    <meta property="og:title" content=".feedback Registry">
-    <meta property="og:type" content="website">
-    <meta property="og:url" content="http://www.nic.feedback">
-
-    <!-- Twitter Card Meta Data -->
-    <meta name="twitter:card" content="summary_large_image">
-    <meta name="twitter:site" content="http://www.nic.feedback/">
-    <meta name="twitter:creator" content="@dotfeedback">
-    <meta name="twitter:title" content=".feedback Registry">
-    <meta name="twitter:description"
-          content=".feedback gTLD Registry">
-    <meta name="twitter:image" content="">
-
-    <!-- Styles -->
-    <link rel="stylesheet" type="text/css" href="//cdn.feedback/css/google_fonts.css">
-    <link rel="stylesheet" type="text/css" href="//cdn.feedback/css/roboto.css">
-    <link rel="stylesheet" type="text/css" href="//cdn.feedback/css/bootstrap-tls-0.1.css">
-    <link rel="stylesheet" type="text/css" href="../assets/nic.feedback.css">
-    <link rel="stylesheet" type="text/css" href="//cdn.feedback/js/typed.css">
-    <link rel="stylesheet" type="text/css" href="//cdn.feedback/js/ladda-0.9.7.min.css">
-</head>
+<?php include("../shared/header.html"); ?>
 
 <body>
 
@@ -236,32 +191,9 @@
 
     <div class="home-hero-bottom" style="">
         <div class="module-customer-logos" style="">
-            <footer>
-                <div class="container">
-                    <div class="row footer-addon">
-                        <div class="col-sm-4 text-left">
-                            <!--<ul class="text-tiny">-->
-                            <!--&lt;!&ndash;  TODO: obfuscate the email &ndash;&gt;-->
-                            <!--<li><a href="mailto:&#099;&#111;&#110;&#116;&#097;&#099;&#116;&#064;&#116;&#111;&#112;&#115;&#112;&#101;&#099;&#116;&#114;&#117;&#109;&#046;&#099;&#111;&#109;" target="_blank">&#099;&#111;&#110;&#116;&#097;&#099;&#116;&#064;&#116;&#111;&#112;&#115;&#112;&#101;&#099;&#116;&#114;&#117;&#109;&#046;&#099;&#111;&#109;</a></li>-->
-                            <!--</ul>-->
-                        </div>
-                        <div class="col-sm-3 legal text-center">
-                            <small class="text-tiny ">
-                                Made with <i class="glyphicon glyphicon-heart"></i> in Seattle<br>
-                                © 2015 Top Level Spectrum, LLC.<br>
-                                All rights reserved.
-                            </small>
-                        </div>
-                        <div class="col-sm-5 text-right">
-                            <!--<ul class="text-tiny">-->
-                            <!--&lt;!&ndash;<li><a href="/terms">Terms</a></li>&ndash;&gt;-->
-                            <!--&lt;!&ndash;<li><a href="/privacy">Privacy</a></li>&ndash;&gt;-->
-                            <!--<li><a href="mailto:&#114;&#101;&#103;&#105;&#115;&#116;&#114;&#121;&#097;&#098;&#117;&#115;&#101;&#064;&#116;&#111;&#112;&#115;&#112;&#101;&#099;&#116;&#114;&#117;&#109;&#046;&#099;&#111;&#109;">Report Abuse</a></li>-->
-                            <!--</ul>-->
-                        </div>
-                    </div>
-                </div>
-            </footer>
+
+           <?php include("shared/footer.html"); ?>
+
         </div>
     </div>
 </section>
