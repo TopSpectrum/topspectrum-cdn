7676c587bf9d7688670a7de8fd4cc1d24b6d6d9a
msmyers <michael@topspectrum.com>
2015-10-19 10:58:32 -0600

    Centralizing the nav

--- nic.feedback/index.php
+++ nic.feedback/index.php
@@ -8,34 +8,7 @@
 <body>
 
 <section class="home-hero">
-    <nav class="navbar" style="position: relative;">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                        data-target="#navbar" aria-expanded="false">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-
-                <a href="/" class="navbar-brand">.feedback</a>
-            </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="navbar">
-                <ul class="nav navbar-nav navbar-right">
-                    <!--<li class="active"><a href="#">Packages</a></li>-->
-                    <!--<li class="active"><a href="#">Customers</a></li>-->
-                    <!--<li class="active"><a href="#">Pricing</a></li>-->
-                    <!--<li class="active"><a href="#">Log In</a></li>-->
-                    <li class="active"><a href="/registrars">Registrars</a></li>
-                    <li class="active"><a href="/contact">Contact</a></li>
-                    <li class="active"><a href="//www.myservice.feedback/login">Login</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+    <?php include("shared/nav.php"); ?>
 
     <div class="home-hero-top container" style="margin-top: 5rem;">
         <div class="row">
