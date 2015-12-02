ea9054028795212d665a09d110a1cf22250ad1ed
msmyers <michael@topspectrum.com>
2015-12-02 09:56:01 -0800

    Rename SocialAccount to Account

--- js/topspectrum-0.1.js
+++ js/topspectrum-0.1.js
@@ -17,6 +17,14 @@ var Ts = {
         return fn;
     },
 
+    loadCss: function(url) {
+        var link = document.createElement("link");
+        link.type = "text/css";
+        link.rel = "stylesheet";
+        link.href = url;
+        document.getElementsByTagName("head")[0].appendChild(link);
+    },
+
     emptyFn: function() {
 
     },
